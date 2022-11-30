695f8c64d58cdda5371d8e6531c7f9e630b08d2b
Tahir <dibirovtahir@gmail.com>
2022-12-01 00:24:04 +0400

    wtf

--- logic.js
+++ logic.js
@@ -11,11 +11,12 @@ submit.addEventListener('click', () => {
     const display = document.querySelector('.container-display')
     player.innerHTML = `Player: ${state.name}`
     highScore.innerHTML = `Score: ${state.highScore}`
+    const easy = document.querySelector('.easy')
+    const medium = document.querySelector('.medium')
+    const hard = document.querySelector('.hard')
 
-    console.log(document.querySelector('.ball'))
+    const startGame = () => {
 
-   const interval = setInterval(() => {
-        
         const ball = document.createElement('div')
         ball.style.position = 'absolute'
         ball.style.marginTop = 'auto'
@@ -23,23 +24,68 @@ submit.addEventListener('click', () => {
         ball.style.left = `${Math.ceil(Math.random() * 300)}px`
         ball.classList.add('ball')
         display.append(ball)
-        document.querySelectorAll('.ball').length === 5 && (clearInterval(interval), display.innerHTML = "Game Over!")
+        document.querySelectorAll('.ball').length === 50 && (clearInterval(interval), display.innerHTML = "Game Over!")
         document.querySelectorAll('.ball').forEach(ball => {
             ball.addEventListener('click', (e) => {
                 e.stopImmediatePropagation()
-                ball.remove() 
+                ball.remove()
                 state.score = state.score + 1
-                
+
                 document.querySelector('.score').innerHTML = `Score: ${state.score}`
 
             })
         })
 
 
-    }, state.speed)
+
+    }
+
+    const interval = setInterval(startGame, state.speed)
+
+    easy.addEventListener('click', () => {
+        state.speed = 1000
+        display.innerHTML = null
+        clearInterval(interval)
+        const easyinterval = setInterval(startGame, state.speed)
+        
+
+    })
+
+    medium.addEventListener('click', ()=>{
+        state.speed = 700
+        display.innerHTML = null
+        clearInterval(interval)
+        const mediuminterval = setInterval(startGame, state.speed)
+    })
+
+
+    hard.addEventListener('click', ()=>{
+        state.speed = 300
+        display.innerHTML = null
+        clearInterval(interval)
+        const hardinterval = setInterval(startGame, state.speed)
+    })
+
+   
 
 
 
 })
 
 
+
+
+
+// console.log(easy)
+
+// easy.addEventListener('click', ()=>{
+//     state.speed = 1000
+// })
+
+// medium.addEventListener('click', ()=>{
+//     state.speed = 700
+// })
+
+// hard.addEventListener('click', ()=>{
+//     state.speed = 400
+// })
